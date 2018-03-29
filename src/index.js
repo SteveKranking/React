63478cabc4bf8c6dc66b18f8c03dba9a6c54595a
Stephen Kranking <stephenkranking@yahoo.com>
2018-03-29 11:19:08 -0400

    encountering strange error, but everything is compiling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDUC75Q4veDTKro0rE8s95ygwcVQkgEJbU';
 
-const App = () => {
-  return <div>
-            <SearchBar />
-          </div>;
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//       </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
