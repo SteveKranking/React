627295bd972b5c3f0cce114286f200433f815061
Stephen Kranking <stephenkranking@yahoo.com>
2018-03-29 12:17:33 -0400

    Video players works! Still some errors in console tough

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDUC75Q4veDTKro0rE8s95ygwcVQkgEJbU';
 
-
-// const App = () => {
-//   return (
-//     <div>
-//       <SearchBar />
-//       </div>
-//   );
-// }
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +14,7 @@ class App extends Component {
     
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'Packers'}, (videos) => {
       this.setState({ videos });
     });
   }
@@ -30,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
